b6d092af6c75b26396ce4d4077fca9733b56f3ce
Paul Harden <paul.harden@trincoll.edu>
2025-07-14 10:15:08 -0400

    Create traffic_light.cpp
    Code to enable the time controlled traffic lights

---
+++ traffic_light.cpp
@@ -0,0 +1,23 @@
+// C++ code
+#define red 12
+#define yellow 11
+#define green 10
+void setup()
+{
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(green, HIGH);
+  delay(30000);
+  digitalWrite(green,LOW);
+  digitalWrite(yellow, HIGH);
+  delay(3000);
+  digitalWrite(yellow,LOW);
+  digitalWrite(red, HIGH);
+  delay(27000);
+  digitalWrite(red,LOW);
+}
